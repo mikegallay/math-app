e894792b55199a0ea566bec03c1549390209d0aa
Michael Gallay <michael.gallay@ogilvy.com>
2019-01-14 23:46:17 -0500

    creatures page start

--- SourceCode/src/app/components/Creatures/Creatures.jsx
+++ SourceCode/src/app/components/Creatures/Creatures.jsx
@@ -8,6 +8,8 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 import './Creatures.scss';
+const creatures = require.context('../../images/creatures', true);
+import {creatureList} from "../../config/constants";
 
 const localUser = "localUser";
 
@@ -22,20 +24,34 @@ export default class Creatures extends React.Component {
 
   }
 
+  renderStyles(operator,id){
+    var imgRef = creatureList.math[operator][id].img;
+    var imgSrc = creatures(`./${imgRef}.png`);
+    return {
+      backgroundImage:'url(' + imgSrc + ')',
+      backgroundSize: 'cover'
+    }
+  }
+
   render() {
     var locUser = JSON.parse(localStorage.getItem(localUser));
 
     return (
       <main className="page-main">
         <h1>Creatures</h1>
-        <Link to='/statistics' className='stats-btn'> View Statistics </Link>
+        <Link to='/statistics' className='stats-btn'> Vadd iew Statistics </Link>
         <span className="active">View Creatures</span>
         <div className="creature-list">
           <div className="creature-section">
             <h2>Addition - Common</h2>
-            <div className="creatures">
-              <div className={`creature ${(locUser.creatures.add.c01)?'gotit':''}`}>C01</div>
-            </div>
+            <ul className="creatures">
+             <li style={this.renderStyles('add','c01')} className="creature">C01</li>
+             <li style={this.renderStyles('add','c02')} className="creature">C02</li>
+              {/*<li className={`creature add c01 ${(locUser.creatures.add.c01)?'gotit':'notyet'}`}>C01</li>
+              <li style={this.renderStyles()} className={`creature add c02 ${(locUser.creatures.add.c02)?'gotit':'notyet'}`}>C02</li>
+              <li className={`creature add c03 ${(locUser.creatures.add.c03)?'gotit':'notyet'}`}>C03</li>
+              <li className={`creature add c02 ${(locUser.creatures.add.c02)?'gotit':'notyet'}`}>C03</li>*/}
+            </ul>
           </div>
         </div>
       </main>
